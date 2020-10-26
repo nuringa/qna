e387fa905827f505849d94520832349318416433
Ekaterina Egorova <admin.karima@gmail.com>
2020-10-26 16:48:02 +0300

    after review spec for #author_of? in user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,27 @@ RSpec.describe User, type: :model do
 
   it { should have_many(:questions) }
   it { should have_many(:answers) }
+
+  describe '#author_of?' do
+    let(:user_author) { create(:user) }
+    let(:another_user) { create(:user) }
+    let(:question) { create(:question, author: user_author) }
+    let(:answer) { create(:answer, author: user_author) }
+
+    it 'should return true if user is author of the question' do
+      expect(user_author.author_of?(question)).to be true
+    end
+
+    it 'should return true if user is author of the answer' do
+      expect(user_author.author_of?(answer)).to be true
+    end
+
+    it 'should return false if user is not author of the question' do
+      expect(another_user.author_of?(question)).to be false
+    end
+
+    it 'should return false if user is not author of the answer' do
+      expect(another_user.author_of?(answer)).to be false
+    end
+  end
 end
