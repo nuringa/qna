b6468a27883ffa379fc0f511bbbda40224b95500
Ekaterina Egorova <admin.karima@gmail.com>
2020-11-04 16:12:00 +0300

    errors render fix && rspec styles fix

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -2,65 +2,68 @@ require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
   let(:user) { create(:user) }
-  let(:question) { create(:question) }
-  let!(:answer) { create(:answer) }
   let(:another_user) { create(:user) }
+  let(:question) { create(:question) }
 
   describe 'POST #create' do
     before { login(user) }
 
+    let(:valid_params) { { question_id: question.id, format: :js, answer: attributes_for(:answer) } }
+
     context 'with valid attributes' do
-      it 'saves a new answer to question answers in database' do
-        expect do
-          post :create, params: { question_id: question.id, format: :js, answer: attributes_for(:answer) }
-        end.to change(question.answers, :count).by(1)
+      it 'saves a new answer to question answers' do
+        expect { post :create, params: valid_params }.to change(question.answers, :count).by(1)
       end
 
       it 'renders create template' do
-        post :create, params: { answer: attributes_for(:answer), format: :js, question_id: question.id }
+        post :create, params: valid_params
         expect(response).to render_template :create
       end
     end
 
     context 'with invalid attributes' do
+      let(:invalid_params) { { question_id: question.id, format: :js, answer: attributes_for(:answer, :invalid) } }
+
       it 'does not save the answer to database' do
-        expect { post :create, params: { answer: attributes_for(:answer, :invalid), format: :js, question_id: question } }.to_not change(Answer, :count)
+        expect { post :create, params: invalid_params }.to_not change(Answer, :count)
       end
 
       it 'renders create template' do
-        post :create, params: { answer: attributes_for(:answer, :invalid), format: :js, question_id: question }
+        post :create, params: invalid_params
         expect(response).to render_template :create
       end
     end
   end
 
   describe 'PATCH #update' do
-    let!(:answer) { create(:answer, question: question, author: question.author) }
-
-    subject { patch :update, params: { id: answer, answer: { body: 'new body' }, format: :js } }
-
     before { login(user) }
 
+    let!(:answer) { create(:answer, question: question, author: question.author) }
+
     context 'with valid attributes' do
+      let(:valid_params) { { id: answer, answer: { body: 'new body' }, format: :js } }
+
       it 'changes answer attributes' do
-        subject
+        patch :update, params: valid_params
         answer.reload
         expect(answer.body).to eq 'new body'
       end
 
       it 'renders update view after save' do
-        subject
+        patch :update, params: valid_params
         expect(response).to render_template :update
       end
     end
 
     context 'with invalid attributes' do
+      let(:invalid_params) { { id: answer, answer: attributes_for(:answer, :invalid) , format: :js } }
+
       it 'does not change answer attributes' do
-        expect { subject }.to_not change(answer, :body)
+        expect { patch :update, params: invalid_params }.to_not change(answer, :body)
       end
 
       it 'renders update view after saving attempt' do
-        subject
+        patch :update, params: invalid_params
         expect(response).to render_template :update
       end
     end
@@ -68,12 +71,14 @@ RSpec.describe AnswersController, type: :controller do
     context 'not author of the answer tries to update it' do
       before { login(another_user) }
 
+      let(:valid_params) { { id: answer, answer: { body: 'new body' }, format: :js } }
+
       it 'does not change answer attributes' do
-        expect { subject }.to_not change(answer, :body)
+        expect { patch :update, params: valid_params }.to_not change(answer, :body)
       end
 
       it 'renders update view' do
-        subject
+        patch :update, params: valid_params
         expect(response).to render_template :update
       end
     end
@@ -82,36 +87,34 @@ RSpec.describe AnswersController, type: :controller do
   describe 'DELETE #destroy' do
     before { login(user) }
 
+    let!(:answer) { create(:answer, author: user, question: question) }
+    let(:valid_params) { { id: answer, format: :js } }
+
     context 'Authenticated user, the author of the answer' do
-
-      let!(:answer) { create(:answer, author: user, question: question) }
-      subject { delete :destroy, params: { id: answer }, format: :js }
-
       it 'deletes the answer' do
-        expect { subject }.to change(Answer, :count).by(-1)
+        expect { delete :destroy, params: valid_params }.to change(Answer, :count).by(-1)
       end
 
       it 'creates a destroy flash message' do
-        subject
+        delete :destroy, params: valid_params
         expect(flash[:danger]).to include('Your answer was deleted.')
       end
 
       it 'renders destroy view after deleting', js: true do
-        subject
+        delete :destroy, params: valid_params
         expect(response).to render_template :destroy
       end
     end
 
     context 'Authenticated user, not author of the answer' do
       before { login(another_user) }
-      subject { delete :destroy, params: { id: answer }, format: :js }
 
       it 'can not delete the answer' do
-        expect { subject }.to_not change(Question, :count)
+        expect { delete :destroy, params: valid_params }.to_not change(Question, :count)
       end
 
       it 'returns forbidden status response' do
-        subject
+        delete :destroy, params: valid_params
         expect(flash[:danger]).to include('Action not allowed')
       end
     end
