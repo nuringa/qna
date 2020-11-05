9d98a918dd4dadf4017d9685244e8d60d095e1eb
Ekaterina Egorova <admin.karima@gmail.com>
2020-11-05 12:04:30 +0300

    edit answer tests && refactor test names

--- spec/features/answer/edit_spec.rb
+++ spec/features/answer/edit_spec.rb
@@ -7,23 +7,21 @@ feature 'User can edit his answer', %q{
 } do
 
   given!(:user) { create(:user) }
+  given(:another_user) { create(:user) }
   given!(:question) { create(:question) }
   given!(:answer) { create(:answer, question: question, author: user) }
 
-  scenario 'Unauthenticated can not edit answer' do
-    visit question_path(question)
-
-    expect(page).to_not have_link 'Edit'
-  end
-
-  describe 'Authenticated user' do
-    scenario 'edits his answer', js: true do
+  describe 'Authenticated user, author of the answer' do
+    background do
       sign_in user
       visit question_path(question)
 
-      click_on 'Edit'
+      within '.answers' do
+        click_on 'Edit'
+      end
+    end
 
-
+    scenario 'edits his answer', js: true do
       within '.answers' do
         fill_in 'Body', with: 'edited answer'
         click_on 'Save'
@@ -34,7 +32,35 @@ feature 'User can edit his answer', %q{
       end
     end
 
-    scenario 'edits his answer with errors'
-    scenario "tries to edit other user's question"
+    scenario 'edits his answer with errors', js: true do
+      within '.answers' do
+        fill_in 'Body', with: ''
+        click_on 'Save'
+
+        expect(page).to have_content answer.body
+        expect(page).to have_content "Body can't be blank"
+        expect(page).to have_selector 'textarea'
+      end
+    end
+  end
+
+  describe 'Authenticated user, not author of the answer' do
+    scenario "tries to edit the answer" do
+      sign_in another_user
+      visit question_path(question)
+
+      within '.answers' do
+        expect(page).to_not have_link 'Edit'
+      end
+    end
+  end
+
+  describe 'Unauthenticated user' do
+    scenario 'can not edit answer' do
+      visit question_path(question)
+      within '.answers' do
+        expect(page).to_not have_link 'Edit'
+      end
+    end
   end
 end
