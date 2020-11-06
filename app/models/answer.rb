6925d836b918e5ad8009c2a7e4b0693deee76816
Ekaterina Egorova <admin.karima@gmail.com>
2020-11-06 09:35:58 +0300

    refactor not using association

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -7,11 +7,9 @@ class Answer < ApplicationRecord
   scope :sort_by_best, -> { order(best: :desc) }
 
   def select_best!
-    former_best = question.answers.find_by(best: true)
-
     transaction do
-      former_best.update!(best: false) if former_best.present?
-      update!(best: true)
+      self.class.where(question_id: self.question_id).update_all(best: false)
+      update(best: true)
     end
   end
 end
