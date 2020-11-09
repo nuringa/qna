f72afc078df87de8364a94164da383c5caf22b38
Ekaterina Egorova <admin.karima@gmail.com>
2020-11-10 00:11:16 +0300

    attachments controller for deleting files && tests

---
+++ app/controllers/active_storage/attachments_controller.rb
@@ -0,0 +1,16 @@
+class ActiveStorage::AttachmentsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :load_attachment
+
+  def destroy
+    return head :forbidden unless current_user.author_of?(@attachment.record)
+
+    @attachment.purge
+  end
+
+  private
+
+  def load_attachment
+    @attachment = ActiveStorage::Attachment.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,8 @@ Rails.application.routes.draw do
       end
     end
   end
+
+  namespace :active_storage do
+    resources :attachments, only: [:destroy]
+  end
 end
