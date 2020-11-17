4b5375c450f6585189b5d79f30a9266f0fbb0173
Ekaterina Egorova <admin.karima@gmail.com>
2020-11-17 21:01:58 +0300

    links url validation

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,4 +2,8 @@ class Link < ApplicationRecord
   belongs_to :linkable, polymorphic: true
 
   validates :name, :url, presence: true
+  validates :url,
+            presence: true,
+            length: { maximum: 245 },
+            format: { with: URI.regexp(%w[http https]), message: 'format is wrong' }
 end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -5,4 +5,15 @@ RSpec.describe Link, type: :model do
 
   it { should validate_presence_of :name }
   it { should validate_presence_of :url }
+  it { should validate_length_of(:url).is_at_most(245) }
+
+  it do
+    should allow_values('https://foo.com', 'http://foo.com').for(:url)
+  end
+
+  it do
+    should_not allow_values('foo', 'foo.com')
+      .for(:url)
+      .with_message('format is wrong')
+  end
 end
