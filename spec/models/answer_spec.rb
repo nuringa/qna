b0612c564cbeb578eb1c03191e36cf184593a115
Ekaterina Egorova <admin.karima@gmail.com>
2020-11-19 19:48:52 +0300

    style fixes && allow_destroy tests for answer and question models

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Answer, type: :model do
   it { should belong_to(:author) }
   it { should have_many(:links).dependent(:destroy) }
 
-  it { should accept_nested_attributes_for :links }
+  it { should accept_nested_attributes_for(:links).allow_destroy(true) }
 
   it { should validate_presence_of :body }
 
